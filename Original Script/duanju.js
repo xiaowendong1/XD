53168416d485cfc0e95e3682bcb80900f506f153
xiaowendong1 <155016372+xiaowendong1@users.noreply.github.com>
2024-02-07 17:25:19 +0800

    Create duanju.js

---
+++ Original Script/duanju.js
@@ -0,0 +1,27 @@
+/*************************************
+
+项目名称：星芽短剧
+下载地址：appstore下载
+脚本作者：xiaowendong1
+使用说明：解锁VIP
+
+**************************************
+[rewrite_local]
+^https:\/\/app\.whjzjx\.cn\/v1\/account\/detail url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/caipu.js
+
+[mitm]
+hostname = app.whjzjx.cn
+
+*************************************/
+
+var body = $response.body;
+
+body = body.replace(/\"expiration_date":""*.*.?""g,'\"expiration_date":9999-12-31');
+
+body = body.replace(/\"is_auto_play_ad": "\w+"/ g,'\"is_auto_play_ad":true');
+
+body = body.replace(/\"vip_status": "\d+"/ g,'\"vip_status":2');
+
+body = body.replace(/\"vip_expired_timestamp": "\d+"/g,'\"vip_expired_timestamp":253402247078');
+
+$done({ body });
