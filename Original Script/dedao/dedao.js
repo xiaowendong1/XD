1d7529544b902759631aa24f342ca1939cf93b62
xiaowendong1 <155016372+xiaowendong1@users.noreply.github.com>
2024-02-17 21:41:39 +0800

    Create dedao.js

---
+++ Original Script/dedao/dedao.js
@@ -0,0 +1,34 @@
+/*************************************到
+项目名称：少年得道
+下载地址：appstore下载（http://985.so/wf7qs）
+脚本作者：xiaowendong1（阿猫阿狗）
+使用说明：解锁付费资源，免费观看，仅限视频和音频
+
+**************************************
+
+[rewrite_local]
+
+^https:\/\/igetcool-gateway\.igetcool\.com url script-response-body dedao.js
+
+[mitm]
+hostname = igetcool-gateway.igetcool.com
+
+var body = $response.body;
+
+const video = '/app-api-product-server/white/course/videos.json';
+
+const Audio  = '/api/product/course/option/course/audios';
+
+if ($request.url.indexOf(video) != -1){
+
+body = body.replace(/\"buy":\d+/g, '\"buy":1');
+
+}
+
+if ($request.url.indexOf(Audio) != -1){
+
+body = body.replace(/\"userAudioLockStatus":\d+/g, '\"userAudioLockStatus":2');
+
+}
+
+$done({body});
