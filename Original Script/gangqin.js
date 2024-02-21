ac52f36bb3425a70584d3047f82b927c1ab2012d
xiaowendong1 <155016372+xiaowendong1@users.noreply.github.com>
2024-02-21 21:31:46 +0800

    更新 gangqin.js

---
+++ Original Script/gangqin.js
@@ -0,0 +1,12 @@
+/*************************************
+
+项目名称：星芽短剧
+下载地址：appstore下载
+脚本作者：xiaowendong1
+使用说明：解锁VIP
+
+**************************************
+[rewrite_local]
+^https:\/\/gangqinpu\.lzjoy\.com\/ url reject-dict
+
+hostname = gangqinpu.lzjoy.com
