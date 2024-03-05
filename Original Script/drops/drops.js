d81efdf0ca8c3779e5b66106583c7348fa23ee39
xiaowendong1 <155016372+xiaowendong1@users.noreply.github.com>
2024-03-05 22:48:50 +0800

    更新 drops.js

--- Original Script/drops/drops.js
+++ Original Script/drops/drops.js
@@ -9,57 +9,36 @@
 
 [rewrite_local]
 
-^https?:\/\/api\.revenuecat\.com\/v1\/(subscribers\/[^\/]+$|receipts$) url script-response-body https://raw.githubusercontent.com/xiaowendong1/XD/main/Original%20Script/drops/drops.js 
+^https?:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-response-body https://raw.githubusercontent.com/xiaowendong1/XD/main/Original%20Script/drops/drops.js 
+^https?:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-request-header https://raw.githubusercontent.com/xiaowendong1/XD/main/Original%20Script/drops/drops.js
 
 [mitm] 
 hostname = api.revenuecat.com
 
 ************************************/
 
-var xd = JSON.parse($response.body);
+const xd1 = {};
+const xd2 = JSON.parse(typeof $response != "undefined" && $response.body || null);
 
-xd = {
-  "request_date_ms" : 1709470947875,
-  "request_date" : "2024-03-03T13:02:27Z",
-  "subscriber" : {
-    "non_subscriptions" : {
+const name = "premium";
+const appid = "premium_yearly_full_price_tier_b_free_trial_7_lang_ko";
 
-    },
-    "first_seen" : "2024-03-03T12:37:35Z",
-    "original_application_version" : "38.8.12",
-    "other_purchases" : {
+if (typeof $response == "undefined") {
+  delete $request.headers["x-revenuecat-etag"];
+  delete $request.headers["X-RevenueCat-ETag"];
+  xd1.headers = $request.headers;
+} else if (xd2 && xd2.subscriber) {
+  xd2.subscriber.subscriptions = xd2.subscriber.subscriptions || {};
+  xd2.subscriber.entitlements = xd2.subscriber.entitlements || {};
 
-    },
-    "management_url" : "https://apps.apple.com/account/subscriptions",
-    "subscriptions" : {
-      "premium_yearly_full_price_tier_b_free_trial_7_lang_ko" : {
-        "original_purchase_date" : "2024-03-03T11:15:28Z",
-        "expires_date" : "2099-03-10T10:15:27Z",
-        "is_sandbox" : false,
-        "refunded_at" : null,
-        "store_transaction_id" : "430001741225607",
-        "unsubscribe_detected_at" : "2024-03-03T11:20:36Z",
-        "grace_period_expires_date" : null,
-        "period_type" : "trial",
-        "purchase_date" : "2024-03-03T11:15:27Z",
-        "billing_issues_detected_at" : null,
-        "ownership_type" : "PURCHASED",
-        "store" : "app_store",
-        "auto_resume_date" : null
-      }
-    },
-    "entitlements" : {
-      "premium" : {
-        "grace_period_expires_date" : null,
-        "purchase_date" : "2024-03-03T11:15:27Z",
-        "product_identifier" : "premium_yearly_full_price_tier_b_free_trial_7_lang_ko",
-        "expires_date" : "2099-03-10T10:15:27Z"
-      }
-    },
-    "original_purchase_date" : "2024-03-03T10:57:10Z",
-    "original_app_user_id" : "us-east-1:36698d82-9ee2-c8df-2a13-af39a42149bf",
-    "last_seen" : "2024-03-03T12:38:11Z"
-  }
+const data = {
+	"product_identifier": (appid),
+	"expires_date": "2099-03-10T10:15:27Z",
+	"purchase_date": "2024-03-03T11:15:27Z"
+	};
+  xd2.subscriber.entitlements[(name)] = (data);
+  xd2.subscriber.subscriptions[(appid)] = {  ...data,	"original_purchase_date": "2024-03-03T11:15:27Z",	"store": "app_store",	"ownership_type": "PURCHASED"};
+  xd1.body = JSON.stringify(xd2);
 }
 
-$done({body: JSON.stringify(xd)});
+$done(xd1);
