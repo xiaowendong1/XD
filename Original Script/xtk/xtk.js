9b29801fa3d8cf755b02331b68db89e62505f1dc
xiaowendong1 <155016372+xiaowendong1@users.noreply.github.com>
2024-03-24 23:41:07 +0800

    Create xtk.js

---
+++ Original Script/xtk/xtk.js
@@ -0,0 +1,31 @@
+/*
+软件名：星题库
+脚本作者：xiaowendong1（xx）
+使用说明：解锁会员，解锁星榜提名
+
+*
+
+[rewrite_local]
+
+^https:\/\/mb\.xinghengedu\.com\/*\/*\/*\.do url reject-dict url script-response-body https://raw.githubusercontent.com/xiaowendong1/XD/main/Original%20Script/xtk/xtk.js
+
+[mitm]
+hostname = mb.xinghengedu.com
+
+*/
+
+var body = $response.body;
+
+body = body.replace(/\"isvip8":\w+/g, '\"isvip8":true');
+
+body = body.replace(/\"isVip":\w+/g, '\"isVip":true');
+
+body = body.replace(/\"vipType":"\d+"/g, '\"vipType":"1"');
+
+body = body.replace(/\"vipStatus":"\d+"/g, '\"vipStatus":"1"');
+
+body = body.replace(/\"vip":\w+/g, '\"vip":true');
+
+$done({ body });
+
+
