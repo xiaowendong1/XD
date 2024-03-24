548a3f8756859ab6625b56ef31b8f4384085eb61
xiaowendong1 <155016372+xiaowendong1@users.noreply.github.com>
2024-03-25 00:35:58 +0800

    Update xtk.js

--- Original Script/xtk/xtk.js
+++ Original Script/xtk/xtk.js
@@ -6,7 +6,8 @@
 **************************************
 [rewrite_local]
 
-^https:\/\/mb\.xinghengedu\.com\/*\/*\/*\.do  url script-response-body https://raw.githubusercontent.com/xiaowendong1/XD/main/Original%20Script/xtk/xtk.js
+^https:\/\/mb\.xinghengedu\.com\/api\/v5\.3\.0\/getUserByToken\.do url script-response-body https://raw.githubusercontent.com/xiaowendong1/XD/main/Original%20Script/xtk/xtk.js
+^https:\/\/mb\.xinghengedu\.com\/questionBank\/v5\.1\.0\/getTopQuestionList\.do url script-response-body https://raw.githubusercontent.com/xiaowendong1/XD/main/Original%20Script/xtk/xtk.js
 
 [mitm]
 hostname = mb.xinghengedu.com
@@ -14,17 +15,22 @@ hostname = mb.xinghengedu.com
 *************************************/
 
 var body = $response.body;
+var xiaodong = JSON.parse(body);
+const xd = '/questionBank/v5.1.0/getTopQuestionList.do';
+const xx = '/api/v5.3.0/getUserByToken.do';
+if ($request.url.indexOf(xd) != -1){
+body = body.replace(/\"isvip8":\w+/g, '\"isvip8":true');
+body = body.replace(/\"isVip":\w+/g, '\"isVip":true');
+}
+if ($request.url.indexOf(xx) != -1){
+xiaodong.data.viptype = "1";
+xiaodong.data.vipStatus = "1";
+xiaodong.data.vip = "true";
+body = JSON.stringify(xiaodong);
+}
 
-body = body.replace(/\"isvip8":"\w+"/g, '\"isvip8":true');
 
-body = body.replace(/\"isVip":"\w+"/g, '\"isVip":true');
+$done({body});
 
-body = body.replace(/\"vipType":"\d+"/g, '\"vipType":"1"');
-
-body = body.replace(/\"vipStatus":"\d+"/g, '\"vipStatus":"1"');
-
-body = body.replace(/\"vip":"\w+"/g, '\"vip":true');
-
-$done({ body: body });
 
 
