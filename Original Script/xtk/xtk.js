04e8e3d39859e08756445bd75f339f9e3e223f4b
xiaowendong1 <155016372+xiaowendong1@users.noreply.github.com>
2024-03-25 11:17:51 +0800

    Update xtk.js

--- Original Script/xtk/xtk.js
+++ Original Script/xtk/xtk.js
@@ -2,7 +2,6 @@
 
 [rewrite_local]
 
-^https:\/\/mb\.xinghengedu\.com\/api\/v5\.3\.0\/getUserByToken\.do url script-response-body https://raw.githubusercontent.com/xiaowendong1/XD/main/Original%20Script/xtk/xtk.js
 ^https:\/\/mb\.xinghengedu\.com\/questionBank\/v5\.1\.0\/getTopQuestionList\.do url script-response-body https://raw.githubusercontent.com/xiaowendong1/XD/main/Original%20Script/xtk/xtk.js
 
 [mitm]
@@ -11,20 +10,8 @@ hostname = mb.xinghengedu.com
 *************************************/
 
 var body = $response.body;
-var xiaodong = JSON.parse(body);
-const xd = '/questionBank/v5.1.0/getTopQuestionList.do';
-const xx = '/api/v5.3.0/getUserByToken.do';
-if ($request.url.indexOf(xd) != -1){
+
 body = body.replace(/\"isvip8":\w+/g, '\"isvip8":true');
 body = body.replace(/\"isVip":\w+/g, '\"isVip":true');
-}
-if ($request.url.indexOf(xx) != -1){
-xiaodong.data.vipStatus = "1";
-xiaodong.data.vipType = "1";
-xiaodong.data.vip = "true";
-body = JSON.stringify(xiaodong);
-}
+
 $done({body});
-
-
-
