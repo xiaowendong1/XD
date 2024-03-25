41977a42e4f93807af4b8638ec18f5440b88b9b5
xiaowendong1 <155016372+xiaowendong1@users.noreply.github.com>
2024-03-25 11:43:04 +0800

    更新 xtk2.js

--- Original Script/xtk2/xtk2.js
+++ Original Script/xtk2/xtk2.js
@@ -10,8 +10,37 @@ hostname = mb.xinghengedu.com
 *************************************/
 let xiaodong = JSON.parse($response.body);
 
-xiaodong.data.vipStatus = "1";
-xiaodong.data.vipType = "1";
-xiaodong.data.vip = "true";
+xiaodong = {
+  "msg" : "操作成功",
+  "code" : 200,
+  "data" : {
+    "key" : "",
+    "username" : "xx",
+    "agentAddress" : "default",
+    "code" : 1,
+    "agentTel" : "4008079767",
+    "img" : "default",
+    "agentName" : " ",
+    "bindPhoneNumber" : "12312341234",
+    "agentQq" : "",
+    "cs" : "",
+    "videoSource" : 0,
+    "endTime" : 0,
+    "crmName" : "",
+    "vipType" : "",
+    "name" : "xx",
+    "info" : "让我们一起加油做题吧~",
+    "id" : 10003014977,
+    "gender" : 0,
+    "vipStatus" : "1",
+    "everstarName" : "",
+    "serverPhone" : "4008079767",
+    "phone" : "",
+    "dedicated" : 0,
+    "vip" : false,
+    "qq" : "",
+    "integral" : 0
+  }
+}
 
 $done({body: JSON.stringify(xiaodong)});
