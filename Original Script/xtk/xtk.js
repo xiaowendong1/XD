8115f7e534ce4db489445961b951e9141f7fb30c
xiaowendong1 <155016372+xiaowendong1@users.noreply.github.com>
2024-03-25 11:03:46 +0800

    Update xtk.js

--- Original Script/xtk/xtk.js
+++ Original Script/xtk/xtk.js
@@ -15,7 +15,7 @@ var xiaodong = JSON.parse(body);
 const xd = '/questionBank/v5.1.0/getTopQuestionList.do';
 const xx = '/api/v5.3.0/getUserByToken.do';
 if ($request.url.indexOf(xd) != -1){
-
+body = body.replace(/\"isvip8":\w+/g, '\"isvip8":true');
 body = body.replace(/\"isVip":\w+/g, '\"isVip":true');
 }
 if ($request.url.indexOf(xx) != -1){
